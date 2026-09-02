6fe5b1626b43c2b4c91aa9a13fc4fe5f65a1f710
106213024 <106213024@student.swin.edu.au>
2026-09-02 09:15:20 +0800

    Added story html, made changes to CSS file.

---
+++ Exercise 3/assets/js/scripts.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const questions = document.querySelectorAll(".faq-question");
+
+    questions.forEach(q => {
+        q.addEventListener("click", () => {
+            const answer = q.nextElementSibling;
+            answer.style.display =
+                answer.style.display === "block" ? "none" : "block";
+        });
+    });
+});
